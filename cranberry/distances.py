3941af763a08bf7596e274d126b7f84e4e0de4c7
Safia Abdalla <seabdalla@gmail.com>
2015-01-02 09:30:37 -0600

    Updated Euclidean distance function

--- cranberry/distances.py
+++ cranberry/distances.py
@@ -1,8 +1,16 @@
 from math import sqrt
+from operator import sub
 
 def euclidean_distance(point_x, point_y):
-	sqrt(abs((point_x[0] - point_y[0]) * (point_x[1] - point_y[1])))
+    return sqrt(pow((point_x[0] - point_y[0]), 2) + 
+                pow((point_x[1] - point_y[1]), 2))
 
 def manhattan_distance(point_x, point_y):
-	pass
+    pass
+
+def hamming_distance(point_x, point_y):
+    pass
+
+def jaccard_distance(point_x, point_y):
+    pass
 
